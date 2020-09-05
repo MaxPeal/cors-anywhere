762a5d08a153f98d347492008d3feef2e5c6cb04
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-09-05 10:00:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -42,5 +42,11 @@ cors_proxy.createServer({
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
-  console.log(cors_proxy.createServer);
+  console.log('originBlacklist ' + originBlacklist);
+  console.log('originWhitelist ' + originWhitelist);
+  console.log('requireHeader ' + requireHeader);
+  console.log('checkRateLimit ' + checkRateLimit);
+  console.log('redirectSameOrigin ' + redirectSameOrigin);
+  console.log('httpProxyOptions ' + httpProxyOptions);
+  console.log(cors_proxy);
 });
